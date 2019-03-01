bdc00360c00e5e4a62549bfc45136665701dd23f
Garrett Johnson <garrett.kjohnson@gmail.com>
2019-02-28 18:33:56 -0800

    Log the sample matches

--- scripts/print-sample-matches.js
+++ scripts/print-sample-matches.js
@@ -1,19 +1,17 @@
 const { getDetailedInfo } = require('../umd/index.js');
 const { database } = require('../umd/database.js');
+const { findMatch } = require('../umd/utils.js');
 const fs = require('fs');
 const path = require('path');
 
-function pad(str, ct) {
-
-    if (ct <= str.length) return str;
-    return pad(str + ' ', ct);
-
-}
-
 const gpus = fs.readFileSync(path.join(__dirname, '../data/sample-unmasked-renderer-data.txt'), 'utf8').trim().split(/\n/g).map(g => g.trim());
-gpus.forEach(g => {
+const dbKeys = Object.keys(database);
+gpus.forEach(name => {
+    
+    let matches = findMatch(name, dbKeys);
+    if (matches.score < 0.75) matches = [];
+    else matches = matches.matches;
 
-    const match = getDetailedInfo(database, g);
-    console.log(pad(g, 80), ' -> ', match && match.name);
+    console.log(name, ' : ', matches.join(', '));
 
 });

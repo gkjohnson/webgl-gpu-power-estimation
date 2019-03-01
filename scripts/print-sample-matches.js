c13ae2c4830ef54ea0e7a71dc1524b42799d1e31
Garrett Johnson <garrett.kjohnson@gmail.com>
2019-02-28 18:43:13 -0800

    Add linting

--- scripts/print-sample-matches.js
+++ scripts/print-sample-matches.js
@@ -1,4 +1,3 @@
-const { getDetailedInfo } = require('../umd/index.js');
 const { database } = require('../umd/database.js');
 const { findMatch } = require('../umd/utils.js');
 const fs = require('fs');
@@ -7,7 +6,7 @@ const path = require('path');
 const gpus = fs.readFileSync(path.join(__dirname, '../data/sample-unmasked-renderer-data.txt'), 'utf8').trim().split(/\n/g).map(g => g.trim());
 const dbKeys = Object.keys(database);
 gpus.forEach(name => {
-    
+
     let matches = findMatch(name, dbKeys);
     if (matches.score < 0.75) matches = [];
     else matches = matches.matches;

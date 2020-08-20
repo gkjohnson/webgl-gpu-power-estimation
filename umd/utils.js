02ff844d116ccac4725fd47f32e03078d1b9222d
Garrett Johnson <garrett.kjohnson@gmail.com>
2020-08-19 19:14:23 -0700

    Revert "remove umd builds, use modules in example"
    This reverts commit e3add6a103741836e2b0dc85167e52547cb6cfc9.

---
+++ umd/utils.js
@@ -0,0 +1,64 @@
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+    typeof define === 'function' && define.amd ? define(['exports'], factory) :
+    (global = global || self, factory(global.GpuPowerEstimate = global.GpuPowerEstimate || {}));
+}(this, function (exports) { 'use strict';
+
+    function strToCompareArray(str) {
+      return str.split(/\W+/g).map(function (c) {
+        return c.trim().toLowerCase();
+      });
+    }
+
+    function compareStr(a, b) {
+      if (typeof a === 'string') a = strToCompareArray(a);
+      if (typeof b === 'string') b = strToCompareArray(b);
+      var matched = [];
+
+      for (var i = 0, l = a.length; i < l; i++) {
+        if (b.includes(a[i])) matched.push(a[i]);
+      }
+
+      var unmatchedTokens = a.length - matched.length + b.length - matched.length;
+      var score = matched.length / Math.min(a.length, b.length) - unmatchedTokens * 0.001;
+      return score;
+    }
+
+    function findMatch(name, list) {
+      var matches = null;
+      var score = -Infinity;
+      var versionMatches = /\w*\d\d\d+\w*/.exec(name);
+      var versionRegexp = null;
+
+      if (versionMatches) {
+        versionRegexp = new RegExp("(^|\\W)".concat(versionMatches[0], "(\\W|$)"), 'i');
+      }
+
+      var gpuArr = strToCompareArray(name);
+
+      for (var i = 0, l = list.length; i < l; i++) {
+        var _name = list[i];
+        if (versionRegexp && !versionRegexp.test(_name)) continue;
+        if (!versionRegexp && /\d\d\d+/.test(_name)) continue;
+        var similarity = compareStr(_name, gpuArr);
+
+        if (similarity > score) {
+          score = similarity;
+          matches = [_name];
+        } else if (similarity === score) {
+          matches.push(_name);
+        }
+      }
+
+      return {
+        matches: matches,
+        score: score
+      };
+    }
+
+    exports.findMatch = findMatch;
+
+    Object.defineProperty(exports, '__esModule', { value: true });
+
+}));
+//# sourceMappingURL=utils.js.map

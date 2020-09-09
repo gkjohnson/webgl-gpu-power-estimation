ca06a24406ec86286f0a18f905c0f7a8c2283aa4
Olli Etuaho <olli.etuaho@gmail.com>
2020-09-09 10:27:22 +0300

    Match optional space inside the GPU model number
    We've seen samples of prefixed GPU model numbers given with both a space inside and no space inside. Examples:
    
    Radeon Pro WX3200 vs.
    Radeon Pro WX 3200
    
    Change the name matching algorithm so that the version number may contain a short uppercase prefix and so that there's an optional space between the prefix and the digits.

--- src/utils.js
+++ src/utils.js
@@ -9,16 +9,37 @@ function compareStr(a, b) {
     if (typeof a === 'string') a = strToCompareArray(a);
     if (typeof b === 'string') b = strToCompareArray(b);
 
-    const matched = [];
-    for (let i = 0, l = a.length; i < l; i++) {
+    if (a.length > b.length) {
 
-        if (b.includes(a[i])) matched.push(a[i]);
+        const temp = a;
+        a = b;
+        b = temp;
 
     }
+    // b is now the longer array.
 
+    let matchedCount = 0;
+    for (let ai = 0, al = a.length; ai < al; ai++) {
 
-    const unmatchedTokens = a.length - matched.length + b.length - matched.length;
-    const score = (matched.length / Math.min(a.length, b.length)) - unmatchedTokens * 0.001;
+        for (let bi = 0, bl = b.length; bi < bl; bi++) {
+
+            if (b[bi] === a[ai]) {
+
+                ++matchedCount;
+                break;
+
+            } else if (bi > 0 && b[bi - 1] + b[bi] === a[ai] && b[bi - 1].length <= 3 && b[bi][0].match(/\d/)) {
+
+                // If the model number has a space in the middle, still treat it as a matching token.
+                matchedCount += 0.9;
+                break;
+
+            }
+        }
+    }
+
+    const unmatchedTokens = a.length - matchedCount + b.length - matchedCount;
+    const score = matchedCount / Math.min(a.length, b.length) - unmatchedTokens * 0.001;
 
     return score;
 
@@ -29,10 +50,25 @@ function findMatch(name, list) {
     let matches = null;
     let score = -Infinity;
 
-    const versionMatches = /\w*\d\d\d+\w*/.exec(name);
+    // We've seen samples of GPU names in both of these formats: "Radeon Pro WX 3200" and "Radeon Pro WX3200".
+    // We consider a version number as having an optional 1-3 letter uppercase prefix followed by at least 3 digits,
+    // and try to match them regardless of if there's a space between the uppercase prefix and the digits.
+    const versionMatch = /(^|\W)([A-Z]{1,3})?\W*(\d\d\d+\w*)/.exec(name);
     let versionRegexp = null;
-    if (versionMatches) {
-        versionRegexp = new RegExp(`(^|\\W)${ versionMatches[0] }(\\W|$)`, 'i');
+    if (versionMatch) {
+
+        if (versionMatch[2] !== undefined) {
+
+            // "WX 3200" or "WX3200"
+            versionRegexp = new RegExp(`(^|\\W)${versionMatch[2]}\\W*${versionMatch[3]}(\\W|$)`, "i");
+
+        } else {
+
+            // Just the digits.
+            versionRegexp = new RegExp(`(^|\\W)${versionMatch[3]}(\\W|$)`, "i");
+
+        }
+
     }
 
     const gpuArr = strToCompareArray(name);
